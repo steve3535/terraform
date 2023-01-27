ad9216a0aaec2ae26d5c8535e2070a2e846d2cbd
mk417 <kwakou.missoh@lalux.lu>
2023-01-27 14:50:25 +0100

    parameterization and refactor v1

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,7 @@
+###################################
+# COMMON INFRA
+###################################
+
 provider "nutanix" {
   username = var.nutanix_username
   password = var.nutanix_password
@@ -49,78 +53,181 @@ provider "vsphere" {
   user = var.vsphere_user 
   password = var.vsphere_password
   allow_unverified_ssl = true
+  #client_debug = true
 }
 
-data "vsphere_datacenter" "vmw_dc" {
+data "vsphere_datacenter" "esx_dc" {
   name = var.vsphere_datacenter
 }
 
-data "vsphere_datastore" "vmw_ds" {
-  name = var.vsphere_datastore
-  datacenter_id = data.vsphere_datacenter.vmw_dc.id
-}
-
-data "vsphere_compute_cluster" "vmw_clus" {
+data "vsphere_compute_cluster" "esx_cluster" {
   name = var.vsphere_cluster
-  datacenter_id = data.vsphere_datacenter.vmw_dc.id
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
 }
 
-data "vsphere_host" "vmw_host" {
-  name = var.vsphere_host
-  datacenter_id = data.vsphere_datacenter.vmw_dc.id
-}
-
-data "vsphere_network" "vmw_net" {
-  name = var.vsphere_network
-  datacenter_id = data.vsphere_datacenter.vmw_dc.id
-}
-
-data "vsphere_content_library" "vmw_lib" {
-  name = var.vsphere_content_library
-}
-
-data "vsphere_resource_pool" "vmw_pool" {
+data "vsphere_resource_pool" "esx_pool" {
   name = var.vsphere_resource_pool
-  datacenter_id = data.vsphere_datacenter.vmw_dc.id
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
 }
 
-data "vsphere_content_library_item" "vmw_lib_item" {
-  name = var.vsphere_content_library_item
+###############################
+# ESX DATASTORES
+###############################
+
+data "vsphere_datastore" "NUT_DMZ_INT_DC1_01" {
+  name = "NUT_DMZ_INT_DC1_01"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_INT_DC2_01" {
+  name = "NUT_DMZ_INT_DC2_01"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_INT_DC1_to_DC2" {
+  name = "NUT_DMZ_INT_DC1_to_DC2"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_INT_DC2_to_DC1" {
+  name = "NUT_DMZ_INT_DC2_to_DC1"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_EXT_DC1_01" {
+  name = "NUT_DMZ_EXT_DC1_01"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_EXT_DC2_01" {
+  name = "NUT_DMZ_EXT_DC2_01"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_EXT_DC1_to_DC2" {
+  name = "NUT_DMZ_EXT_DC1_to_DC2"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_datastore" "NUT_DMZ_EXT_DC2_to_DC1" {
+  name = "NUT_DMZ_EXT_DC2_to_DC1"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+###############################
+# ESX HOSTS
+###############################
+
+data "vsphere_host" "nut-dmz-01" {
+  name = "nut-dmz-01.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-02" {
+  name = "nut-dmz-02.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-03" {
+  name = "nut-dmz-03.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-04" {
+  name = "nut-dmz-04.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-05" {
+  name = "nut-dmz-05.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-06" {
+  name = "nut-dmz-06.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-07" {
+  name = "nut-dmz-07.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_host" "nut-dmz-08" {
+  name = "nut-dmz-08.lalux.local"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+
+###############################
+# ESX NETWORKS
+###############################
+
+data "vsphere_network" "DMZ_DKV_WEB" {
+  name = "DMZ_DKV_WEB"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_network" "DMZ_MGMT_VLAN20" {
+  name = "DMZ_MGMT_VLAN20"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_network" "DMZ_MGMT_INT" {
+  name = "DMZ_MGMT_INT"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+data "vsphere_network" "DMZ_MGMT_EXT" {
+  name = "DMZ_MGMT_EXT"
+  datacenter_id = data.vsphere_datacenter.esx_dc.id
+}
+
+
+###############################
+# CONTENT LIBRARY
+###############################
+
+data "vsphere_content_library" "esx_lib" {
+  name = "Linux_Templates_DC2"
+}
+
+data "vsphere_content_library_item" "esx_lib_item" {
+  name = "RHEL8STD"
   type = "ovf"
-  library_id = data.vsphere_content_library.vmw_lib.id 
-}
-
-data "template_file" "cloud-init" {
-  template = file("user-data.yml")
-  vars = {
-    vm_user          = var.vm_user
-    vm_name          = var.vm_name
-    vm_ip            = var.vm_ip
-    vm_domain        = var.vm_domain
-    vm_netmask       = var.vm_ipv4_net_prefix
-    vm_gateway       = var.vm_gateway
-    vm_dns1          = var.vm_dns1
-    vm_dns2          = var.vm_dns2
-    vm_public_key    = var.vm_public_key
-    vm_satellite_env = var.vm_satellite_env
-  }
+  library_id = data.vsphere_content_library.esx_lib.id 
 }
 
 
-resource "nutanix_virtual_machine" "LU718" {
-  name                 = "LU718 - LNX - KWAKOU VM TEST"
+# data "template_file" "cloud-init" {
+#   template = file("user-data.yml")
+#   vars = {
+#     vm_user          = var.vm_user
+#     vm_domain        = var.vm_domain
+#     vm_dns1          = var.vm_dns1
+#     vm_dns2          = var.vm_dns2
+#     vm_public_key    = var.vm_public_key
+#     vm_name          = var.lan_vm[0].name 
+#     vm_ip            = var.lan_vm[0].ip
+#     vm_netmask       = var.lan_vm[0].net_prefix
+#     vm_gateway       = var.lan_vm[0].gw
+#     vm_satellite_env = var.lan_vm[0].satellite_env
+#   }
+# }
+
+resource "nutanix_virtual_machine" "MK417-AHV-TEST1" {
+  name                 = "MK417 - LNX - KWAKOU VM TEST 01"
   description          = "VM de TEST"
   provider             = nutanix.dc1
   boot_type            = "UEFI"
-  cluster_uuid         = data.nutanix_cluster.cluster650.metadata.uuid
-  num_vcpus_per_socket = var.vm_num_vcpus_per_socket
-  num_sockets          = var.vm_cpu
-  memory_size_mib      = var.vm_ram
+  cluster_uuid         = data.nutanix_cluster.cluster480.metadata.uuid
+  num_vcpus_per_socket = var.lan_vm[0].cpu_socket
+  num_sockets          = var.lan_vm[0].cpu
+  memory_size_mib      = var.lan_vm[0].mem
 
   nic_list {
-    #subnet_uuid = "5e33cdaf-d482-4353-b2b0-a74cbff387c8" #200.1.1. Production sur LU651
-    subnet_uuid = "0cd904d5-187c-4701-ad94-12bda719dcac"    #200.1.1. Production sur LU650
-    #subnet_uuid = "e261a1ee-cc02-48ff-9292-92287a9c95ec"    #192.168.25
+    subnet_uuid = var.ahv_480_network["Production"]
+    
   }
 
   disk_list {
@@ -140,25 +247,31 @@ resource "nutanix_virtual_machine" "LU718" {
   }
 
   disk_list {
-    #defining an additional entry in the disk_list array will create another.
-    disk_size_mib = (var.vm_disk_size_gb * 1024)
-    # disk_size_bytes = (var.vm_disk_size_gb * 1024 * 1024)
-
+    disk_size_mib = (var.lan_vm[0].disk2_size_gb * 1024)
 
     storage_config {
       storage_container_reference {
         kind = "storage_container"
-        #uuid = "bc8f3bc4-900a-4458-aadd-85f557f5bcd2" # NUT-AHV-DC3-01
-        uuid = "68f3f950-143e-41a9-87fa-6806dfaacaa8" # NUT-AHV-DC1-01
+        uuid = var.ahv_480_storage["NUT_AHV_LU480_DC01_01"]
       }
     }
   }
-
-
-  guest_customization_cloud_init_user_data = base64encode(data.template_file.cloud-init.rendered)
+  
+  #guest_customization_cloud_init_user_data = base64encode(data.template_file.cloud-init.rendered)
+   guest_customization_cloud_init_user_data = base64encode(templatefile("user-data.tpl", {
+    vm_domain         =  var.vm_domain
+    vm_name       =  var.lan_vm[0].name
+    vm_ip   = var.lan_vm[0].ip
+    vm_prefix = var.lan_vm[0].net_prefix
+    vm_gateway   =  var.lan_vm[0].gw
+    vm_dns1    = var.vm_dns1
+    vm_dns2    = var.vm_dns2
+    vm_user = var.vm_user
+    vm_public_key = var.vm_public_key
+  })) 
 
   provisioner "local-exec" {
-    command = " ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${data.template_file.cloud-init.vars.vm_name},' -e env=${data.template_file.cloud-init.vars.vm_satellite_env} deploy.yml -u localadmin -b --vault-password-file /home/lalux.local/mk417/infrastructure-linux/vault/.vault_password_file"
+    command = " ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${var.lan_vm[0].name},' -e env=${var.lan_vm[0].satellite_env} deploy.yml -u ${var.vm_user} -b --vault-password-file /home/lalux.local/mk417/infrastructure-linux/vault/.vault_password_file"
 
   }
 
@@ -166,86 +279,86 @@ resource "nutanix_virtual_machine" "LU718" {
 
 
 # Deploy a VM from OVF in Content Library in VMWARE
-resource "vsphere_virtual_machine" "TEST-LNX" {
-  name                 = var.vsphere_machine_name
-  host_system_id       = data.vsphere_host.vmw_host.id
-  datastore_id         = data.vsphere_datastore.vmw_ds.id
-  resource_pool_id     = data.vsphere_resource_pool.vmw_pool.id
-  folder               = var.vsphere_folder
-  firmware             = "efi"
-  num_cpus             = var.vsphere_num_cpus
-  memory               = var.vsphere_memory
-  wait_for_guest_net_timeout = 5
+# resource "vsphere_virtual_machine" "MK417-ESX-TEST1" {
+#   resource_pool_id     = data.vsphere_resource_pool.esx_pool.id
+#   host_system_id       = data.vsphere_host.nut-dmz-08.id
+#   datastore_id         = data.vsphere_datastore.NUT_DMZ_INT_DC2_to_DC1.id 
+#   firmware             = "efi"
+#   name                 = var.dmz_vm[0].name 
+#   folder               = var.dmz_vm[0].folder
+#   num_cpus             = var.dmz_vm[0].cpu
+#   memory               = var.dmz_vm[0].mem 
+#   wait_for_guest_net_timeout = 5
 
-  disk {
-    label            = "disk0"
-    size             = 50
-    controller_type  = "scsi"
-  }
-  disk {
-    label            = "disk1"
-    size             = 100
-    controller_type  = "scsi"
-    unit_number      = 1
-  }
-  cdrom {
-  }
+#   disk {
+#     label            = "disk0"
+#     size             = 50
+#     controller_type  = "scsi"
+#   }
+#   disk {
+#     label            = "disk1"
+#     size             = 100
+#     controller_type  = "scsi"
+#     unit_number      = 1
+#   }
+#   cdrom {
+#   }
 
-  clone {
-    template_uuid = data.vsphere_content_library_item.vmw_lib_item.id
-    customize {
-      linux_options {
-        host_name = var.vsphere_vm_name
-        domain    = "lalux.local"
-      }
-      # Nécessaire malgré la config nmcli via le prov remote-exec car justement remote-exec a besoin d'une IP pour se connecter  
-      network_interface {
-        ipv4_address = cidrhost(var.vsphere_subnet,20) #fixed IP address .20
-        ipv4_netmask = 24
+#   clone {
+#     template_uuid = data.vsphere_content_library_item.esx_lib_item.id
+#     customize {
+#       linux_options {
+#         host_name = var.dmz_vm[0].name
+#         domain    = "lalux.local"
+#       }
+#       # Nécessaire malgré la config nmcli via le prov remote-exec car justement remote-exec a besoin d'une IP pour se connecter  
+#       network_interface {
+#         ipv4_address = cidrhost(var.dmz_vm[0].subnet,53) #fixed IP address .20
+#         ipv4_netmask = 24
         
-      }
-      ipv4_gateway = cidrhost(var.vsphere_subnet,1)
-      dns_server_list = ["200.1.1.163","200.1.1.218"]
-    }
-  }
+#       }
+#       ipv4_gateway = cidrhost(var.dmz_vm[0].subnet,240)
+#       dns_server_list = ["200.1.1.163","200.1.1.218"]
+#     }
+#   }
 
-  network_interface {
-    network_id = data.vsphere_network.vmw_net.id
-  }
+#   network_interface {
+#     network_id = data.vsphere_network.DMZ_MGMT_VLAN20.id
+#   }
 
-  provisioner "file" {
-    source = "/home/lalux.local/mk417/.ssh/id_rsa.pub"
-    destination = "/tmp/authorized_keys"
-    connection {
-      type = "ssh"
-      user = "localadmin"
-      password = var.vm_password
-      host = var.vsphere_vm_name
-    }
-  }
+#   provisioner "file" {
+#     source = "/home/lalux.local/mk417/.ssh/id_rsa.pub"
+#     destination = "/tmp/authorized_keys"
+#     connection {
+#       type = "ssh"
+#       user = "localadmin"
+#       password = var.vm_password
+#       host = var.dmz_vm[0].name
+#     }
+#   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "mkdir /home/localadmin/.ssh",
-      "chmod 0700 /home/localadmin/.ssh",
-      "mv /tmp/authorized_keys /home/localadmin/.ssh/",
-      "chmod 0600 /home/localadmin/.ssh/authorized_keys",      
-      "sudo nmcli con mod 'System ${var.vsphere_interface_name}' ipv4.method manual ipv4.addresses ${var.vsphere_vm_ip}/24 ipv4.gateway ${var.vsphere_vm_gateway} connection.autoconnect yes",
-      "sudo nmcli con mod 'System ${var.vsphere_interface_name}' con-name ${var.vsphere_interface_name}",
-      "sudo nmcli con up ${var.vsphere_interface_name}",
-      "sudo dnf -y remove cloud-init"      
-    ]
-    connection {
-      type = "ssh"
-      user = "localadmin"
-      password = var.vm_password
-      host = var.vsphere_vm_name
-    }
+#   provisioner "remote-exec" {
+#     inline = [
+#       "mkdir /home/localadmin/.ssh",
+#       "chmod 0700 /home/localadmin/.ssh",
+#       "mv /tmp/authorized_keys /home/localadmin/.ssh/",
+#       "chmod 0600 /home/localadmin/.ssh/authorized_keys",      
+#       "sudo nmcli con mod 'System ${var.vsphere_interface_name}' ipv4.method manual ipv4.addresses ${var.dmz_vm[0].ip}/24 ipv4.gateway ${var.dmz_vm[0].gateway} connection.autoconnect yes",
+#       "sudo nmcli con mod 'System ${var.vsphere_interface_name}' con-name ${var.vsphere_interface_name}",
+#       "sudo nmcli con up ${var.vsphere_interface_name}",
+#       "sudo dnf -y remove cloud-init"      
+#     ]
+#     connection {
+#       type = "ssh"
+#       user = "localadmin"
+#       password = var.vm_password
+#       host = var.dmz_vm[0].name
+#     }
     
-  }
+#   }
   
-  provisioner "local-exec" {
-    command = " ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${var.vsphere_vm_name},' -e env=${var.vsphere_vm_satellite_env} deploy.yml -u localadmin -b --vault-password-file /home/lalux.local/mk417/infrastructure-linux/vault/.vault_password_file"
-  }
+#   provisioner "local-exec" {
+#     command = " ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${var.dmz_vm[0].name},' -e env=${var.dmz_vm[0].satellite_env} deploy.yml -u localadmin -b --vault-password-file /home/lalux.local/mk417/infrastructure-linux/vault/.vault_password_file"
+#   }
 
-}
+# }
