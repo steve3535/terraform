5e5f24b4a94c48886acdf61122e978e1553fe815
mk417 <kwakou.missoh@lalux.lu>
2023-03-27 15:35:44 +0200

    testpipe

--- dev/convert_to_csv.py
+++ dev/convert_to_csv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # Replace 'input_file.xlsx' with your Excel file name
-input_file = 'vmdefs.xlsx'
+input_file = '/opt/infrastructure-linux/terraform/dev/vmdefs.xlsx'
 # Replace 'output_file.csv' with your desired CSV output file name
-output_file = 'vmdefs.csv'
+output_file = '/opt/infrastructure-linux/terraform/dev/vmdefs.csv'
 
 # Read the Excel file
 df = pd.read_excel(input_file, engine='openpyxl')
