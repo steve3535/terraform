56265dd6acfd5cbc90453b50d7318933d12c66d1
mk417 <kwakou.missoh@lalux.lu>
2023-05-10 12:54:23 +0200

    sw parse jobs to ctrs

--- dev/xlx2csv/convert_to_csv2.py
+++ dev/xlx2csv/convert_to_csv2.py
@@ -2,9 +2,9 @@
 import pandas as pd
 
 # Replace 'input_file.xlsx' with your Excel file name
-input_file = '/opt/infrastructure-linux/terraform/dev/vmdefs.xlsx'
+input_file = '/var/tmp/in.xlsx'
 # Replace 'output_file.csv' with your desired CSV output file name
-output_file = '/opt/infrastructure-linux/terraform/dev/vmdefs.csv'
+output_file = '/var/tmp/out.csv'
 
 # Read the Excel file
 df = pd.read_excel(input_file, engine='openpyxl')
