ff5b7ceb9128e47b279cd2f3a11edb7b3ec2d400
mk417 <kwakou.missoh@lalux.lu>
2024-12-04 17:28:14 +0100

    some changes [skip ci]

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ data "nutanix_cluster" "pe_lu651" {
   provider = nutanix.dc3
 }
 
-# data "nutanix_image" "rhel8-dc3" {
-#   image_name = "RHEL8STD-latest"
-#   provider   = nutanix.dc3
-# }
+data "nutanix_image" "rhel8-dc3" {
+  image_name = "RHEL8STD-latest"
+  provider   = nutanix.dc3
+}
 
 provider "nutanix" {
   username = var.nutanix_username
@@ -148,6 +148,7 @@ data "vsphere_host" "nut-dmz-08" {
   datacenter_id = data.vsphere_datacenter.esx_dc.id
 }
 
+
 ###############################
 # ESX NETWORKS
 ###############################
@@ -237,6 +238,10 @@ data "vsphere_network" "PPR_APPLICATIONS_LN" {
   datacenter_id = data.vsphere_datacenter.esx_dc.id
 }
 
+# data "vsphere_network" "PPR_MIGAL" {
+#   name = "PPR_MIGAL"
+#   datacenter_id = data.vsphere_datacenter.esx_dc.id
+# }
 
 data "vsphere_network" "PPR_MAGIC" {
   name = "PPR_MAGIC"
@@ -298,44 +303,6 @@ data "vsphere_network" "DMZ_EXT_Oracle_Mgmt" {
   datacenter_id = data.vsphere_datacenter.esx_dc.id
 }
 
-
-data "vsphere_network" "PRO_ANALYTICS_EXT" {
-  name = "PRO_ANALYTICS_EXT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-data "vsphere_network" "REC_PUBLIC_SITE_EXT" {
-  name = "REC_PUBLIC_SITE_EXT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-data "vsphere_network" "PRO_PUBLIC_SITE_EXT" {
-  name = "PRO_PUBLIC_SITE_EXT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-
-data "vsphere_network" "PRO_AUTH_EXT" {
-  name = "PRO_AUTH_EXT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-
-data "vsphere_network" "PRO_AUTH_INT" {
-  name = "PRO_AUTH_INT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-data "vsphere_network" "REC_AUTH_EXT" {
-  name = "REC_AUTH_EXT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
-data "vsphere_network" "REC_AUTH_INT" {
-  name = "REC_AUTH_INT"
-  datacenter_id = data.vsphere_datacenter.esx_dc.id
-}
-
 ###############################
 # CONTENT LIBRARY
 ###############################
@@ -359,5 +326,59 @@ data "vsphere_content_library_item" "esx_lib2_item" {
   type = "ovf"
   library_id = data.vsphere_content_library.esx_lib2.id 
 }
+# BEGIN ANSIBLE MANAGED BLOCK VSL-PRO-IDM-001
+resource "nutanix_virtual_machine" "VSL-PRO-IDM-001" {
+        name                 = "VSL-PRO-IDM-001"
+        description          = "IDM REPLICA" 
+        provider             = nutanix.dc3
+        cluster_uuid         = data.nutanix_cluster.pe_lu651.metadata.uuid
+        num_vcpus_per_socket = "1"
+        num_sockets          = "4"
+        memory_size_mib      = "8192"
+        boot_type            = "UEFI"
+        nic_list {
+          subnet_uuid = var.ahv_651_network["New_PROD 192.168.25.x"]
+         }
 
+        disk_list {
+          data_source_reference = {
+             kind = "image"
+             uuid = data.nutanix_image.rhel8-dc3.metadata.uuid
+          }
 
+          device_properties {
+            disk_address = {
+              device_index = 0
+              adapter_type = "SCSI"
+            }
+            device_type = "DISK"
+          }
+        }
+
+        disk_list {
+          disk_size_mib = (50 * 1024)
+          storage_config {
+            storage_container_reference {
+              kind = "storage_container"
+              uuid = var.ahv_651_storage["NUT_AHV_DC3_01"]
+            }
+          }
+        }
+
+        guest_customization_cloud_init_user_data = base64encode(templatefile("user-data.yaml", {
+          vm_domain         =  var.vm_domain 
+          vm_name       =  "vsl-pro-idm-001"
+          vm_ip   = "192.168.25.28"
+          vm_prefix = "24"
+          vm_gateway   =  "192.168.25.1"
+          vm_dns1    = var.vm_dns1
+          vm_dns2    = var.vm_dns2
+          vm_user = var.vm_user
+          vm_public_key = var.public_key
+        }))
+
+        provisioner "local-exec" {
+        command = " ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i 'vsl-pro-idm-001,' -e env=DEV_TEST config.yml -u ${var.vm_user} -b --vault-password-file /opt/infrastructure/linux/vault/.vault_password_file" 
+        }
+ }
+# END ANSIBLE MANAGED BLOCK VSL-PRO-IDM-001
